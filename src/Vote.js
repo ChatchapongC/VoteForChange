349fadd0e6223d3b48f721eb3ebd071aa97009fc
Chatchapong Chumpada <chatchapong.c@ku.th>
2020-05-26 23:09:09 +0700

    Fix

--- src/Vote.js
+++ src/Vote.js
@@ -8,7 +8,7 @@ import { Button } from 'react-bootstrap';
 const campaignlist = [
     {
         id: 1,
-        image:"https://pixabay.com/p-848904/?no_redirect",
+        image:"default_image.jpeg",
         name: 'Free Nazanin Ratcliffe',
         description: 'My wife Nazanin Zaghari-Ratcliffe, 37 year old charity worker, was on holiday visiting her family in Iran. She was at the airport returning to the UK on the 3rd April when she was detained by the Iranian Revolutionary Guard. She has been transferred to an unknown location in Kerman Province',
         votes: 0,
@@ -16,15 +16,15 @@ const campaignlist = [
     },
     {
         id: 2,
-        image:'../public/default_image.jpeg',
+        image:"default_image.jpeg",
         name: 'Save the environment - Stop giving plastic toys with fast food kids meals',
         description:'We are Ella and Caitlin aged 10 and 8, and we’ve been learning all about the environment at school and the problem of plastic. It made us very sad to see how plastic harms wildlife and pollutes the ocean, and we want to change this. That’s why we want Burger King and McDonald’s to think of the environment and stop giving plastic toys with their kids meals.',
         votes: 0,
-        goal: 9000
+        goal: 8000
     },
     {
         id: 3,
-        image:'../public/default_image.jpeg',
+        image:'default_image.jpeg',
         name: 'Stop taxing periods. Period. #EndTamponTax',
         description:"We need to make sure we no longer tax period products on luxurious, “non-essential” grounds, but not helicopters, the maintenance of our private jets, or crocodile steaks. Even President Obama has coined the tax 'shocking' and 'unfair'.",
         votes: 0,
@@ -32,7 +32,7 @@ const campaignlist = [
     },
     {
         id: 4,
-        image:'../public/default_image.jpeg',
+        image:'default_image.jpeg',
         name: 'UNFREEZE NHS Annual pay rise - Pay us our dues',
         description:'The NHS treats more than 5 million people every year. This year has been the most arduous one to date within our lifetimes. The COVID-19 pandemic hit the whole of the UK hard. Key workers stepped up from many different vocations and skill sets to serve and protect; the NHS being the juggernaut behind the operational safety of the UK',
         votes: 0,
@@ -40,7 +40,7 @@ const campaignlist = [
     },
     {
         id: 5,
-        image:'../public/default_image.jpeg',
+        image:'default_image.jpeg',
         name: 'Government must provide all teaching staff with adequate PPE',
         description:"I am a primary school teacher and a mother of 3 young children. The government will soon reopen primary schools during this pandemic. They have said that teachers don't need PPE.",
         votes: 0,
@@ -107,8 +107,8 @@ class Campaign extends Component {
     </Card>
             {this.props.description}    
         </tr>
-            <ProgressBar striped variant='warning' max={this.props.goal} now={this.props.votes} label={`${this.props.votes}/`+`${this.props.goal}`}  />
-            <Button variant='warning' onClick={this.handleClick}>Vote this campaign</Button> 
+            <ProgressBar striped variant='warning' max={this.props.goal} now={500} label={500 +` voted of / ${this.props.goal} goal`}  />
+            {/* <Button variant='warning' onClick={this.handleClick}>Vote this campaign</Button>  */}
     </div>
     );}
 }
