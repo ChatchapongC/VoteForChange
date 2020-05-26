cede4046acffeb164d57615e2692e4cd812213f0
Chatchapong Chumpada <chatchapong.c@ku.th>
2020-05-26 16:11:59 +0700

    Add Voting function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "../src/components/Header";
+import Vote from './Vote';
+import Home from './components/Home';
+import {BrowserRouter as Router, Switch, Route} from'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={Home}/>
+        <Route path="/vote" component={Vote}/>
+      </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,297 @@
+import React, { Component } from "react";
+import Modal from "react-responsive-modal";
+import "./css/style.css";
+import {
+  Link
+} from 'react-router-dom'
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sign: false,
+      login: false,
+    };
+  }
+
+  onOpenModal = () => {
+    this.setState({ sign: true });
+  };
+
+  onOpenModalLogin = () => {
+    this.setState({ login: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ sign: false });
+  };
+
+  onCloseModalclose = () => {
+    this.setState({ login: false });
+  };
+
+
+  render() {
+    const { login, sign } = this.state;
+    return (
+      <>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
+          rel="stylesheet"
+        ></link>
+        <header
+          className="header header-animated opaque"
+          style={{
+            display: "block",
+            paddingTop: "5px",
+            paddingBottom: "5px",
+            background: "rgba(243, 227, 227)",
+            border: "1px solid #FFD3D3",
+          }}
+        >
+          <div className="container">
+            <nav className="navbar navbar-default" role="navigation">
+              <div className="navbar-header">
+                <a className="logo" href="#">
+                  <svg
+                    width="46"
+                    height="48"
+                    viewBox="0 0 46 48"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.67548 23.8893L21.9622 0.624314C22.1982 0.20809 22.5991 -1.4414e-05 23 0C23.4009 1.4413e-05 23.8018 0.208119 24.0378 0.624314L36.3245 23.8893C36.6708 24.4999 36.4888 25.2945 35.9175 25.6656L23.6308 33.6474C23.4371 33.7733 23.2186 33.8362 23 33.8362C22.7815 33.8362 22.563 33.7733 22.3692 33.6474L10.0825 25.6656C9.77519 25.466 9.58048 25.1437 9.52011 24.7935C9.46827 24.4927 9.51548 24.1714 9.67548 23.8893Z"
+                      fill="#3D3D3D"
+                    />
+                    <path
+                      d="M11.4784 29.4292L22.3859 36.262C22.5755 36.3807 22.7878 36.4401 23 36.4401C23.2123 36.4401 23.4245 36.3807 23.6141 36.262L34.5216 29.4292C35.0849 29.0763 35.7011 29.7924 35.3237 30.3615L23.9903 47.4532C23.7485 47.8177 23.3743 48 23 48C22.6258 48 22.2515 47.8177 22.0097 47.4532L10.6763 30.3615C10.2989 29.7924 10.9151 29.0763 11.4784 29.4292Z"
+                      fill="#3D3D3D"
+                    />
+                    <path
+                      d="M23 37.1513L23 47.25M10.25 24.7502L23 18.6324M23 18.6324L35.75 24.7502M23 18.6324L23 0.75M23 18.6324V33M5.75 23.25L1.25 20.25M5.75 26.25L1.25 29.25M40.25 23.25L44.75 20.25M40.25 26.25L44.75 29.25"
+                      stroke="white"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+
+                  <a
+                    style={{
+                      color: "#3D3D3D",
+                      "font-family": "'Lobster', cursive",
+                      "font-size": "36px",
+                      position: "absolute",
+                      "margin-left": "15px",
+                    }}
+                  >
+                    Voteforchange
+                  </a>
+                </a>
+              </div>
+
+              <div
+                className="nav-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#navbarMain"
+                aria-expanded="false"
+                style={{ top: "15px" }}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+              <div
+                className="navbar-collapse collapse in"
+                id="navbarMain"
+                aria-expanded="true"
+                style={{ top: "65px" }}
+              >
+                <ul className="nav navbar-nav navbar-right">
+                  <li>
+                    <button
+                      className="btn btn-primary-outline"
+                      id="signup"
+                      onClick={this.onOpenModal}
+                    >
+                      SignUp
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="btn btn-primary-outline"
+                      id="login"
+                      onClick={this.onOpenModalLogin}
+                    >
+                      Login
+                    </button>
+                  </li>
+                </ul>
+                <ul className="nav navbar-nav collapsed-color">
+                  <li>
+                  <Link to="/">Home</Link>
+                  </li>
+
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+
+                  <li>
+                    <a href="#">Features</a>
+                  </li>
+
+                  <li>
+                    <a href="#">Blog</a>
+                  </li>
+                  <li>
+                    <a href="#">Products</a>
+                  </li>
+                </ul>
+              </div>
+
+              {/* <!-- .navbar-collapse --> */}
+            </nav>
+          </div>
+        </header>
+        {/* Sign up model */}
+
+        <Modal open={sign} onClose={this.onCloseModal}>
+          <div className="modal-body">
+            <h2>
+              Get Started Absolutely<span> Free!</span>
+            </h2>
+            <span className="subtitle">Just fill the form below</span>
+            <form
+              className="contact-form form-validate3"
+              novalidate="novalidate"
+            >
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  type="text"
+                  name="fname"
+                  id="fname"
+                  placeholder="First Name"
+                  required=""
+                  autocomplete="off"
+                  aria-required="true"
+                  onChange = {(event,newValue) => this.setState({fname:newValue})}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  type="text"
+                  name="lname"
+                  id="lname"
+                  placeholder="Last Name"
+                  required=""
+                  autocomplete="off"
+                  aria-required="true"
+                  onChange = {(event,newValue) => this.setState({lname:newValue})}
+                />
+                </div>
+                <div className="form-group">
+                <input
+                  className="form-control"
+                  type="text"
+                  name="id_card"
+                  id="id_card"
+                  placeholder="ID Card No./ Passport No."
+                  required=""
+                  autocomplete="off"
+                  aria-required="true"
+                  onChange = {(event,newValue) => this.setState({idCard:newValue})}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  type="email"
+                  name="email"
+                  placeholder="E-mail"
+                  required=""
+                  autocomplete="off"
+                  aria-required="true"
+                  onChange = {(event,newValue) => this.setState({email:newValue})}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  name="pass"
+                  className="form-control"
+                  placeholder="Password"
+                  required=""
+                  autocomplete="off"
+                  aria-required="true"
+                  onChange = {(event,newValue) => this.setState({password:newValue})}
+                />
+              </div>
+              <input
+                className="btn btn-md btn-primary btn-center"
+                id="sign_up"
+                type="button"
+                value="Sign Up"
+                onClick={(event) => this.handleClick(event)}/>
+              />
+            </form>
+          </div>
+        </Modal>
+
+        {/* <!-- signUp End -->
+                  <!-- login --> */}
+
+        <Modal open={login} onClose={this.onCloseModalclose}>
+          <div className="modal-body">
+            <h2>
+              Login and Get <span>Started</span>
+            </h2>
+            <span className="subtitle"></span>
+            <form
+              className="contact-form form-validate4"
+              novalidate="novalidate"
+            >
+              <div className="form-group">
+                <input
+                  className="form-control"
+                  type="email"
+                  name="email"
+                  placeholder="E-mail"
+                  required=""
+                  autocomplete="off"
+                  aria-required="true"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  name="pass"
+                  className="form-control"
+                  placeholder="Password"
+                  required=""
+                  autocomplete="off"
+                  aria-required="true"
+                />
+              </div>
+              <input
+                className="btn btn-md btn-primary btn-center"
+                id="login_btn"
+                type="button"
+                value="Login"
+                onClick = ""
+              />
+            </form>
+          </div>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default Header;
